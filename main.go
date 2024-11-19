872f93306591e08ba3e8be1a101eef4db72cdbe3
Yousef Mahmoud <usef.mahmoud@outlook.com>
2024-11-19 23:15:16 +0200

    learn about variables and loops

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World")
+    var name string = "yousef"
+    println("the name is ", name)
+
+    full_name := name + " mahmoud"
+    fmt.Println("the full name is %s", full_name)
+
+    // while loop
+    i := 0
+    for i < 10{
+        println("Current line is: ", i)
+        i++
+    }
+
+    // normal for loop
+    for i := 1; i <= 10; i++ {
+        fmt.Printf("the new line: %d\n", i)
+    }
+
 }
+
+
