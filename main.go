62cddaa7cd1e97ebc2d162879774320d491d843f
Yousef Mahmoud <usef.mahmoud@outlook.com>
2024-11-21 06:21:37 +0200

    create the core structures

--- main.go
+++ main.go
@@ -1,26 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-    var name string = "yousef"
-    println("the name is ", name)
-
-    full_name := name + " mahmoud"
-    fmt.Println("the full name is %s", full_name)
-
-    // while loop
-    i := 0
-    for i < 10{
-        println("Current line is: ", i)
-        i++
-    }
-
-    // normal for loop
-    for i := 1; i <= 10; i++ {
-        fmt.Printf("the new line: %d\n", i)
-    }
-
+type Task struct {
+	id       int
+	contenct string
+	status   bool
 }
 
+type List struct {
+	title string
+	tasks []Task
+}
 
+func main() {
+	var ToDO List
+	ToDO.title = "My To-Do List"
+
+	fmt.Printf("Welcome To %s App\n", ToDO.title)
+}
